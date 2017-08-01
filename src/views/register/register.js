8c8ec9e60835077ca574bd3643ce11918c4872d7
Joseph Abell <joseph.abell@piksel.com>
2017-08-01 11:33:48 +0100

    register form validation and error handling

--- src/views/register/register.js
+++ src/views/register/register.js
@@ -1,5 +1,6 @@
 import './register.css';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { firebaseConnect } from 'react-redux-firebase';
 import GoogleButton from 'react-google-button';
 import { Control, Form } from 'react-redux-form';
@@ -18,6 +19,10 @@ class Register extends Component {
     this.loginWithGoogle = this.loginWithGoogle.bind(this);
     this.loginWithTwitter = this.loginWithTwitter.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      error: ''
+    }
   }
 
   handleSubmit(val) {
@@ -26,6 +31,8 @@ class Register extends Component {
     firebase.createUser({
       email: val.email,
       password: val.password
+    }).catch(error => {
+      this.setState({ error: error.message });
     });
   }
 
@@ -35,8 +42,10 @@ class Register extends Component {
     firebase.login({
       provider: 'google',
       type: 'popup'
+    }).catch(error => {
+      this.setState({ error: error.message });
     });
-  }i
+  }
 
   loginWithFacebook() {
     const { firebase } = this.props;
@@ -44,6 +53,8 @@ class Register extends Component {
     firebase.login({
       provider: 'facebook',
       type: 'popup'
+    }).catch(error => {
+      this.setState({ error: error.message });
     });
   }
 
@@ -53,11 +64,26 @@ class Register extends Component {
     firebase.login({
       provider: 'twitter',
       type: 'popup'
+    }).catch(error => {
+      this.setState({ error: error.message });
     });
   }
 
-
   render() {
+    const { submitFailed, formErrors, emailErrors, passwordErrors, passwordConfirmErrors } = this.props;
+    const displayBlock = {display: 'block'};
+    const displayNone = {display: 'none'};
+
+    const isError = this.state.error || (submitFailed && (formErrors.passwordsMatch || emailErrors.required || emailErrors.isEmail || passwordErrors.required || passwordConfirmErrors.required));
+
+    const firebaseErrorStyle = this.state.error ? displayBlock : displayNone;
+    const formErrorStyle = isError ? displayBlock : displayNone;
+    const passwordsMatchErrorStyle = (submitFailed && formErrors.passwordsMatch) ? displayBlock : displayNone;
+    const emailRequiredErrorStyle = (submitFailed && emailErrors.required) ? displayBlock : displayNone;
+    const isEmailErrorStyle = (submitFailed && emailErrors.isEmail) ? displayBlock : displayNone;
+    const passwordRequiredErrorStyle = (submitFailed && passwordErrors.required) ? displayBlock : displayNone;
+    const passwordConfirmRequiredErrorStyle = (submitFailed && passwordConfirmErrors.required) ? displayBlock : displayNone;
+
     return (
       <div className="register">
         <h1>Register</h1>
@@ -71,16 +97,30 @@ class Register extends Component {
                 passwordConfirm: { required }
               }}
         >
-          <label htmlFor="email">Email:</label>
-          <Control type='email' model='.email' />
-
-          <label htmlFor="password">Password:</label>
-          <Control type='password' model='.password' />
-
-          <label htmlFor="passwordConfirm">Confirm Password:</label>
-          <Control type='password' model='.passwordConfirm' />
-
-          <button>Submit</button>
+          <div style={formErrorStyle}>
+            <h2>Error</h2>
+            <p>You have one or more errors below</p>
+            <div style={firebaseErrorStyle}>{ this.state.error }</div>
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <Control type='email' model='.email' />
+            <div style={emailRequiredErrorStyle}>An Email is required.</div>
+            <div style={isEmailErrorStyle}>Please provide a valid email.</div>
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <Control type='password' model='.password' />
+            <div style={passwordRequiredErrorStyle}>Please provide a password.</div>
+            <div style={passwordsMatchErrorStyle}>Your passwords do not match.</div>
+          </div>
+          <div>
+            <label htmlFor="passwordConfirm">Confirm Password:</label>
+            <Control type='password' model='.passwordConfirm' />
+            <div style={passwordConfirmRequiredErrorStyle}>Please provide a second password.</div>
+            <div style={passwordsMatchErrorStyle}>Your passwords do not match.</div>
+          </div>
+         <button>Submit</button>
         </Form>
 
         <GoogleButton onClick={() => {
@@ -103,4 +143,13 @@ class Register extends Component {
   }
 }
 
-export default firebaseConnect()(Register);
+const mapStateToProps = (state) => ({
+  submitFailed: state.forms.register.$form.submitFailed,
+  formErrors: state.forms.register.$form.errors,
+  emailErrors: state.forms.register.email.errors,
+  passwordErrors: state.forms.register.password.errors,
+  passwordConfirmErrors: state.forms.register.passwordConfirm.errors
+});
+
+const wrappedRegister = firebaseConnect()(Register);
+export default connect(mapStateToProps)(wrappedRegister);
