6dbeb5d1b1ac1db05e87f75e42ba72d66f327bc2
Joseph Abell <jsphabll@gmail.com>
2017-08-02 07:31:51 +0100

    Update protectedRoute.test.js

--- src/components/protectedRoute/protectedRoute.test.js
+++ src/components/protectedRoute/protectedRoute.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './Login';
+import ProtectedRoute from './ProtectedRoute';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Login firebase={} />, div);
+  ReactDOM.render(<ProtectedRoute />, div);
 });
