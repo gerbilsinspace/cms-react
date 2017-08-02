1da07662ad9ffad5ba7d3ac4edbf5a6866508cd8
Joseph Abell <jsphabll@gmail.com>
2017-08-02 07:33:17 +0100

    Update cms.test.js

--- src/views/cms/cms.test.js
+++ src/views/cms/cms.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './Login';
+import CMS from './CMS';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Login firebase={} />, div);
+  ReactDOM.render(<CMS />, div);
 });
