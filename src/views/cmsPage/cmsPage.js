f0aa87079c34df7c329821673f9eff1c7c55ff3a
Joseph Abell <jsphabll@gmail.com>
2017-08-12 16:52:08 +0100

    ORY editor works, but now I'm failing to get the editor to accept state from firebase. I think it is because the editor is getting a different id.

--- src/views/cmsPage/cmsPage.js
+++ src/views/cmsPage/cmsPage.js
@@ -3,20 +3,20 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firebaseConnect } from 'react-redux-firebase';
 import Editor, { Editable, createEmptyState } from 'ory-editor-core';
-import 'ory-editor-core/lib/index.css';
 import { Trash, DisplayModeToggle, Toolbar } from 'ory-editor-ui';
-import 'ory-editor-ui/lib/index.css';
 import slate from 'ory-editor-plugins-slate';
-import 'ory-editor-plugins-slate/lib/index.css';
 import parallax from 'ory-editor-plugins-parallax-background';
-import 'ory-editor-plugins-parallax-background/lib/index.css';
 import spacer from 'ory-editor-plugins-spacer';
-import 'ory-editor-plugins-spacer/lib/index.css';
 import video from 'ory-editor-plugins-video';
-import 'ory-editor-plugins-video/lib/index.css';
 import image from 'ory-editor-plugins-image';
-import 'ory-editor-plugins-image/lib/index.css';
 import divider from 'ory-editor-plugins-divider';
+import 'ory-editor-core/lib/index.css';
+import 'ory-editor-ui/lib/index.css';
+import 'ory-editor-plugins-slate/lib/index.css';
+import 'ory-editor-plugins-parallax-background/lib/index.css';
+import 'ory-editor-plugins-spacer/lib/index.css';
+import 'ory-editor-plugins-video/lib/index.css';
+import 'ory-editor-plugins-image/lib/index.css';
 
 require('react-tap-event-plugin')();
 
@@ -25,7 +25,7 @@ const plugins = {
   layout: [parallax({ defaultPlugin: slate() })]
 };
 
-const content = createEmptyState();
+let content = createEmptyState();
 
 const editor = new Editor({
   plugins,
@@ -34,13 +34,52 @@ const editor = new Editor({
 });
 
 class Page extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editorState: {},
+      pageId: ''
+    };
+
+    this.onUpdate = this.onUpdate.bind(this);
+    this.onSave = this.onSave.bind(this);
+  }
+
+  onUpdate(state) {
+    this.setState({
+      id: state.pageId,
+      editorState: state
+    });
+  }
+
+  onSave(state) {
+    const { firebase, pageId } = this.props;
+
+    firebase.update(`/pages/${pageId}/`, {
+      'editorState': JSON.stringify(state.editorState)
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const editorState = JSON.parse(nextProps.page.editorState);
+
+    if (editorState) {
+      content = editorState;
+    }
+  }
+
   render() {
     return (
       <div>
         <div className="editorHeader">
-          <h1>Editing</h1>
+          <h1>Editing
+            <button onClick={() => {
+              this.onSave(this.state);
+            }}>Save</button>
+          </h1>
         </div>
-        <Editable editor={editor} id={content.id} />
+        <Editable editor={editor} id={content.id} onChange={this.onUpdate} />
         <Trash editor={editor}/>
         <DisplayModeToggle editor={editor}/>
         <Toolbar editor={editor}/>
@@ -51,13 +90,15 @@ class Page extends Component {
 
 const mapStateToProps = state => {
   let page = null;
+  const pageId = state.router.location.pathname.replace('/cms/pages/', '');
 
-  if (state.firebase.data.page) {
-    page = state.firebase.data.pages[state.router.location.pathname.replace('/cms/pages/', '')]
+  if (state.firebase.data.pages) {
+    page = state.firebase.data.pages[pageId]
   }
 
   return {
-    page: page
+    page,
+    pageId
   };
 };
 
