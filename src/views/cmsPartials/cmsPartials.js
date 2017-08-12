335420f1ef86ae6779d9c8fc38a358377791f764
Joseph Abell <jsphabll@gmail.com>
2017-08-12 08:57:34 +0100

    use CMSList component to create partials page

--- src/views/cmsPartials/cmsPartials.js
+++ src/views/cmsPartials/cmsPartials.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
+import { actions } from 'react-redux-form';
+import { compose } from 'redux';
+import CMSList from '../../components/cmsList';
 
-const Partials = () => (
-  <div></div>
+const Partials = ({
+  items,
+  submitFailed,
+  nameErrors,
+  firebase,
+  clearItemAction
+}) => (
+  <CMSList items={items}
+           submitFailed={submitFailed}
+           nameErrors={nameErrors}
+           firebase={firebase}
+           clearItemAction={clearItemAction}
+           itemNameLowercase='partial'
+           itemNameUppercase='Partial'
+           itemNameUppercasePlural='Partials'
+           itemFirebaseUrl='/partials'
+  />
 );
 
-export default Partials;
+const mapStateToProps = state => ({
+  items: state.firebase.ordered.partials,
+  submitFailed: state.forms.createPartial.$form.submitFailed,
+  nameErrors: state.forms.createPartial.name.errors
+});
+
+const mapDispatchToProps = dispatch => ({
+  clearItemAction: () => {
+    dispatch(actions.change('createPartial.name', ''));
+  }
+});
+
+export default compose(
+  firebaseConnect([
+    'partials#orderByChild=name'
+  ]),
+  connect(mapStateToProps, mapDispatchToProps)
+)(Partials);
