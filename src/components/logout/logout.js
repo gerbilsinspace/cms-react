c80450dd1e311f802130a2490bb5eb23f5b15b77
Joseph Abell <jsphabll@gmail.com>
2017-08-19 16:43:08 +0100

    Add logout button to unauthorised page; redirect to login on pressing logout button

--- src/components/logout/logout.js
+++ src/components/logout/logout.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import { firebaseConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class Logout extends Component {
   constructor(props) {
     super(props);
     this.logout = this.logout.bind(this);
+
+    this.state = {
+      redirect: false
+    }
   }
 
   logout() {
     const { firebase } = this.props;
 
-    firebase.logout();
+    firebase.logout().then(() => {
+      this.setState({
+        redirect: true
+      });
+    });
   }
 
   render() {
+    if (this.state.redirect) {
+      return (
+        <Redirect to='/login' />
+      );
+    }
+
+
     return (
       <div className="logout">
         <button onClick={() => {
@@ -24,4 +42,12 @@ class Logout extends Component {
   }
 }
 
-export default firebaseConnect()(Logout);
+const mapStateToProps = state => {
+  return {};
+};
+
+export default compose(
+  firebaseConnect(),
+  connect(mapStateToProps)
+)(Logout);
+
--- src/components/unauthorised/unauthorised.js
+++ src/components/unauthorised/unauthorised.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logout from '../logout';
 
 const Unauthorised = () => (
   <div>
     <h1>Awaiting Authorisation</h1>
     <p>In order to make edits to this website, you need an admin to give you permission.</p>
+    <Logout />
   </div>
 );
 
